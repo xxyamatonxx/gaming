e3f3350f8244ec15d0f93537d3c40fa984a4a338
xxyamatonxx <xxyamatonxx@gmail.com>
2020-11-08 05:55:21 +0000

    トップコントローラー関連

---
+++ resources/views/top.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<video id="my-video" width="400px" autoplay muted playsinline></video>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'TopController@index')->name('top');
 
 
 Route::get('/auth/{service}', 'OAuthLoginController@getTwitterAuth')->where('service', 'twitter');
